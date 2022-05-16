373171c91d6940fd4544deadaa24f3b2d4def215
mikaildokgoz <mikaildokgoz@gmail.com>
2022-05-16 21:19:41 +0300

    updated

--- fscohort/apps.py
+++ fscohort/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class FschortConfig(AppConfig):
+class FscohortConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'fschort'
+    name = 'fscohort'
